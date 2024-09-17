8c30ffed9aa18d695e12f32defa5725f52ed0b0c
Mickael Simon <mickael.simon60@gmail.com>
2024-09-17 16:53:20 +0200

    Working Avalon 6.0 with Primefaces 13

---
+++ src/main/java/org/primefaces/avalon/PrimeFacesTestApplicationConfig.java
@@ -0,0 +1,10 @@
+package org.primefaces.avalon;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class PrimeFacesTestApplicationConfig {
+
+}
